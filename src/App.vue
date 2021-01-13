eb9ab85fdc3d687398f3852a81460c1ca449476d
pavlosti <pavlostif@gmail.com>
2021-01-13 12:02:59 +0200

    add route, add bootstrap, add axios to handle http requests

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- Nav bar -->
+    <nav class="navbar navbar-custom navbar-dark justify-content-between flex-nowrap flex-row">
+      <div class="container">
+        <a class="navbar-brand float-left">MEVN Stack ToDo App</a>
+        <ul class="nav navbar-nav flex-row float-right">
+          <li class="nav-item">
+            <router-link class="nav-link pr-3" to="/">Create Student</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/view">View Students</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <!-- Router view -->
+    <div class="container mt-5">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -21,6 +29,25 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+
+/* Modify the backgorund color */ 
+.navbar-custom { 
+    background-color: #42b983; 
+} 
+
+
+#nav a.router-link-exact-active {
+  color: #ffeedb;
 }
 </style>
